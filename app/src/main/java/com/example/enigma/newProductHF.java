ff54dce3618660998a2ae328b094b9677f487a9c
Resara <resarapamudithavcc@gmail.com>
2020-10-02 01:49:03 +0530

    Add newProductHF.java

---
+++ app/src/main/java/com/example/enigma/newProductHF.java
@@ -0,0 +1,71 @@
+package com.example.enigma;
+
+public class Product {
+    private int id;
+    private String title, shortdesc;
+    private double rating;
+    private double price;
+    private int image;
+
+    public Product(int id, String title, String shortdesc, double rating, double price, int image) {
+        this.id = id;
+        this.title = title;
+        this.shortdesc = shortdesc;
+        this.rating = rating;
+        this.price = price;
+        this.image = image;
+    }
+
+    public Product(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getShortdesc() {
+        return shortdesc;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setShortdesc(String shortdesc) {
+        this.shortdesc = shortdesc;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
+
